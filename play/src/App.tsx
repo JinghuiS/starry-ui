87af10069676e4413e9ebbb100a6b748e29d87a6
石京辉 <1292077619@qq.com>
2023-03-08 21:37:22 +0800

    feat: 🎸 增加starryLoading指令

--- play/src/App.tsx
+++ play/src/App.tsx
@@ -3,22 +3,21 @@ import { type Component, createSignal } from "solid-js";
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 import { Button, Input, Tag, Textarea } from "@starry-ui/components";
-import { tooltip } from "@starry-ui/directives";
+import { tooltip, starryLoading } from "@starry-ui/directives";
 
 const App: Component = () => {
   const [loading, setLoading] = createSignal(false);
 
   return (
     <div class={styles.App}>
-      <Button>1</Button>
-      <div
-        use:tooltip={{
-          content: "111",
+      <Button
+        onClick={() => {
+          setLoading(!loading());
         }}
       >
-        11
-      </div>
-      <div style={{ width: "300px" }}>
+        1
+      </Button>
+      <div use:starryLoading={{ isShow: loading() }} style={{ width: "300px" }}>
         <Input showCount maxLength={5} />
         <Textarea showCount maxLength={5} style={{ "margin-top": "20px" }} />
       </div>
