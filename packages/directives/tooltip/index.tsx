77a9d836a2575957af3d6c4e6232a7cdbc4ff04b
石京辉 <1292077619@qq.com>
2023-03-08 23:07:55 +0800

    feat: 🎸 增加tooltip可以传递组件

---
+++ packages/directives/tooltip/index.tsx
@@ -0,0 +1,51 @@
+import { type JSX, onMount, onCleanup } from "solid-js";
+import { renderToString, render } from "solid-js/web";
+import tippy, { type Instance, type Props } from "tippy.js";
+
+interface TooltipProps extends Partial<Omit<Props, "content">> {
+  content: JSX.Element;
+}
+
+declare module "solid-js" {
+  namespace JSX {
+    interface Directives {
+      tooltip: TooltipProps;
+    }
+  }
+}
+
+function tooltip(el: Element, props: () => TooltipProps) {
+  let instance: Instance<Props>;
+
+  let contentBox: HTMLElement;
+
+  onMount(() => {
+    const { content, ..._props } = props();
+    contentBox = document.createElement("div");
+    render(() => <>{content}</>, contentBox);
+    instance = tippy(el, {
+      ..._props,
+      content: contentBox,
+      maxWidth: 250,
+      appendTo: () => document.body,
+      animation: "shift-away-subtle",
+      interactive: true,
+      onShow(_instance) {
+        _instance.setContent(contentBox || "");
+        const node = document.getElementsByTagName("html")[0];
+        if (node.classList.contains("starry-dark")) {
+          _instance.popper.children[0].setAttribute("data-theme", "dark");
+        } else {
+          _instance.popper.children[0].setAttribute("data-theme", "light");
+        }
+      },
+    });
+    instance.popper.children[0].setAttribute("data-starry", "tooltip");
+  });
+  onCleanup(() => {
+    //@ts-ignore
+    instance = null;
+  });
+}
+
+export { tooltip };
